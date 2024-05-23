60d3404029eaa0dba8edc37cd8039b8c67a3c3d8
shibaramdas-git <shibaram.daseng@gmail.com>
2024-05-23 12:11:45 +0530

    Contact page basic styling and responsiveness completed

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,12 +4,37 @@ import { useForm } from "react-hook-form";
 
 const StyledContact = styled.div`
   padding: 80px 6%;
-  div:first-child {
-    display: flex;
+  font-size: 1.5rem;
+  a {
+    color: black;
+    text-decoration: none;
+    &:hover {
+      text-decoration: underline;
+    }
   }
-  img {
-    height: 250px;
-    width: 95%;
+`;
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  figure {
+    width: 40%;
+    margin-top: 1em;
+
+    img {
+      height: 50%;
+      width: 100%;
+      margin-bottom: 1em;
+      border-radius: 8px;
+      box-shadow:
+        rgba(0, 0, 0, 0.25) 0px 10px 20px,
+        rgba(0, 0, 0, 0.22) 0px 10px 10px;
+    }
+  }
+  @media (max-width: 1024px) {
+    flex-direction: column;
+    figure {
+      width: 100%;
+    }
   }
 `;
 const Hello = styled.h1`
@@ -20,12 +45,71 @@ const Hello = styled.h1`
     font-size: 65px;
   }
 `;
-const Contactfrom = styled.form`
-  width: 50%;
-  margin-top: 1em;
+const Consent = styled.div`
+  font-size: 1rem;
+  input {
+    margin-top: 2em;
+    margin-right: 1em;
+  }
 `;
-const Input = styled.div``;
-const Erromsg = styled.p``;
+const Contactfrom = styled.form`
+  margin-top: 1em;
+  width: 50%;
+  button {
+    font-size: 1.2rem;
+    padding: 10px;
+    color: white;
+    border: none;
+    border-radius: 8px;
+    background-color: #2683fc;
+    &:hover {
+      background-color: #0057c8;
+    }
+  }
+  input[type="submit"] {
+    border: none;
+    background: none;
+    font-size: inherit;
+    width: 100%;
+    display: block;
+    text-align: right;
+    margin-top: 2em;
+    &:hover {
+      color: #535353;
+    }
+  }
+  @media (max-width: 1024px) {
+    width: 100%;
+  }
+`;
+const Input = styled.div`
+  margin-bottom: 1em;
+  input {
+    margin-top: 0.5em;
+    font-size: 1.5rem;
+    font-weight: lighter;
+    color: gray;
+    width: 100%;
+    padding: 8px;
+    border: none;
+    border-bottom: 2px solid lightgray;
+    /* background: yellow; */
+    &:focus {
+      outline: none;
+      border-bottom: 2px solid gray;
+    }
+  }
+  input[type="checkbox"] {
+    width: 40px;
+    height: 1em;
+  }
+`;
+
+const Erromsg = styled.p`
+  color: red;
+  font-size: 0.95rem;
+  padding-top: 8px;
+`;
 function Contact() {
   const {
     register,
@@ -38,19 +122,13 @@ function Contact() {
   return (
     <StyledContact>
       <Hello>Say Hello</Hello>
-      <div>
+      <Wrapper>
         <figure>
           <img
             src="https://img.freepik.com/premium-vector/contact-us-customer-support-hotline-people-connect-businessman-using-laptop-touching-virtual-screen-contact-icons-email-address-live-chat-internet-wifi_43780-8623.jpg"
             alt=""
           />
-          <figcaption>
-            {" "}
-            or Write us at{" "}
-            <a href="mailto: alev123@gmail.com">alev123@gmail.com</a>
-          </figcaption>
         </figure>
-
         <Contactfrom onSubmit={handleSubmit(onSubmitFun)}>
           <Input>
             <div>
@@ -117,29 +195,28 @@ function Contact() {
             />
             {errors.project && <Erromsg>{errors.project.message}</Erromsg>}
           </Input>
-          <Input>
-            <div>
-              <input
-                type="checkbox"
-                id="consent"
-                {...register("consent", {
-                  required: {
-                    value: true,
-                    message: "Required",
-                  },
-                })}
-              />
-              <label htmlFor="consent">
-                I have read and agree with <strong>Privacy policy</strong> &{" "}
-                <strong>Terms of Services</strong>
-              </label>
-            </div>
-
+          <Consent>
+            <input
+              type="checkbox"
+              id="consent"
+              {...register("consent", {
+                required: {
+                  value: true,
+                  message: "Required",
+                },
+              })}
+            />
+            <span>
+              I have read and agree with <strong>Privacy policy</strong> &{" "}
+              <strong>Terms of Services</strong>
+            </span>
             {errors.consent && <Erromsg>{errors.consent.message}</Erromsg>}
-          </Input>
-          <button>Send</button>
+          </Consent>
+          <div>
+            <input type="submit" value="~ Send" />
+          </div>
         </Contactfrom>
-      </div>
+      </Wrapper>
     </StyledContact>
   );
 }
